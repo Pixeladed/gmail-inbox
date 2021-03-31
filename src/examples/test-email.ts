9ab06fc62d6c21f0042d95aeb35bcd9203a52581
Minh <minhle@canva.com>
2021-03-31 17:21:38 +1100

    skip tests

--- src/examples/test-email.ts
+++ src/examples/test-email.ts
@@ -2,30 +2,30 @@ import * as path from 'path';
 import { Inbox } from '../Inbox';
 import { SearchQuery } from '../SearchQuery.interface';
 
-(async () => {
-  const inbox = new Inbox(path.resolve(__dirname, 'credentials.json'));
+// (async () => {
+//   const inbox = new Inbox(path.resolve(__dirname, 'credentials.json'));
 
-  await inbox.authenticateAccount();
-  console.log('My labels', await inbox.getAllLabels());
-  console.log('My inbox', await inbox.getLatestMessages());
-  console.log("send yourself a mail with the subject 'test' and the content 'test' :)");
-  console.log(
-    'My test mail',
-    await inbox.waitTillMessage({
-      filenameExtension: 'pdf', // note: the filenames containing pdf e.g. 'not-a-pdf.png' will also be returned
-      has: 'attachment', // must have an attachment
-      mustContainText: 'verify', // either subject or message must contain string test
-      newerThan: {
-        // only of today
-        amount: 1,
-        period: 'day',
-      },
-      before: {
-        // must be before now
-        date: new Date(),
-        precision: 'milliseconds',
-      },
-      subject: 'welcome', // subject must contain string test
-    } as SearchQuery),
-  );
-})();
+//   await inbox.authenticateAccount();
+//   console.log('My labels', await inbox.getAllLabels());
+//   console.log('My inbox', await inbox.getLatestMessages());
+//   console.log("send yourself a mail with the subject 'test' and the content 'test' :)");
+//   console.log(
+//     'My test mail',
+//     await inbox.waitTillMessage({
+//       filenameExtension: 'pdf', // note: the filenames containing pdf e.g. 'not-a-pdf.png' will also be returned
+//       has: 'attachment', // must have an attachment
+//       mustContainText: 'verify', // either subject or message must contain string test
+//       newerThan: {
+//         // only of today
+//         amount: 1,
+//         period: 'day',
+//       },
+//       before: {
+//         // must be before now
+//         date: new Date(),
+//         precision: 'milliseconds',
+//       },
+//       subject: 'welcome', // subject must contain string test
+//     } as SearchQuery),
+//   );
+// })();
