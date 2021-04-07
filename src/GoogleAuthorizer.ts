73b38cf3b19afc5b307390dd74c144afbb0ce911
Minh <minhle@canva.com>
2021-04-07 11:43:31 +1000

    add flow for no refresh token

--- src/GoogleAuthorizer.ts
+++ src/GoogleAuthorizer.ts
@@ -1,5 +1,6 @@
 import { Credentials, OAuth2Client } from 'google-auth-library';
 import { google } from 'googleapis';
+import * as readline from 'readline';
 
 export interface ClientCredentials {
   client_id: string;
@@ -17,18 +18,52 @@ export const authorizeAccount = async (credentials: ClientCredentials, refreshTo
 
   auth.setCredentials({ refresh_token: refreshToken })
 
-  const newCredentials = await new Promise<Credentials>(async (resolve, reject) => {
-    auth.refreshAccessToken((error, cred) => {
-      if (error) {
-        return reject(error)
-      } else {
-        if (cred) {
-          resolve(cred)
-        }
-      }
-    })
-  })
+  let newCredentials: Credentials;
 
+  try {
+    newCredentials = await new Promise<Credentials>(async (resolve, reject) => {
+      auth.refreshAccessToken((error, cred) => {
+        if (error) {
+          return reject(error)
+        } else {
+          if (cred) {
+            resolve(cred)
+          }
+        }
+      })
+    })
+  } catch (error) {
+    console.error('errored whole refreshing token', error)
+    console.log('getting new token code instead')
+    newCredentials = await getNewToken(auth)
+  }
   auth.setCredentials(newCredentials);
   return auth;
+};
+
+const getNewToken = async (oAuth2Client: OAuth2Client): Promise<any> => {
+  const scopes = ['https://www.googleapis.com/auth/gmail.readonly'];
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: 'offline',
+    prompt: 'consent',
+    scope: scopes,
+  });
+  console.log('Authorize this app by visiting this url:', authUrl);
+  const readLine = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  return new Promise((resolve, reject) => {
+    readLine.question('Enter the code from that page here: ', async code => {
+      readLine.close();
+      oAuth2Client.getToken(code, (err, token) => {
+        if (err) {
+          reject(err);
+        } else {
+          console.log('received token', token)
+          resolve(token);
+        }
+      });
+    });
+  });
 };
